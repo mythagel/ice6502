f23e97e409fb28f207218af79ef6e1dd2e0113f2
Nicholas Gill <mythagel@gmail.com>
2018-06-09 22:36:44 +1000

    wip

--- w65c816.py
+++ w65c816.py
@@ -58,9 +58,9 @@ import glob
 # Y            Crystal or Oscillator
 # Z            Ref Des Suppressed
 
+# Setup
 set_default_tool(KICAD)
 lib_search_paths[KICAD] = [
-    os.path.abspath('lib/65xx_Library/KiCad'),
     os.path.abspath('lib/kicad-libs'),
     os.path.abspath('lib/KiCad-Schematic-Symbol-Libraries'),
     os.path.abspath('lib/kicad-symbols'),
@@ -70,7 +70,7 @@ lib_search_paths[KICAD] = [
 # local parts
 local = SchLib(name='local')
 
-# LD112 linear regulator part
+# LD112 linear regulator
 ldl112 = Part(name='LDL112_SO8', tool=SKIDL, dest=TEMPLATE)
 ldl112.ref_prefix = 'VR'
 ldl112.description = '1.2 A low quiescent current LDO'
@@ -84,13 +84,11 @@ ldl112 += Pin(num=7, name='GND', func=Pin.PWRIN)
 ldl112 += Pin(num=8, name='NC', func=Pin.NOCONNECT)
 local += ldl112
 
-
 # global nets
 gnd = Net('GND')
 vin = Net('VIN')
 supply_3v3 = Net('3v3')
 supply_1v2 = Net('1v2')
-
 
 # 3v3 supply switching regulator + filters
 @subcircuit
@@ -100,9 +98,9 @@ def reg3v3(vin, vout):
     vreg.ref_prefix = 'VR'
     inC = Part('Device', 'C', value='10uF/50V', footprint='C_1210_HandSoldering')
     outC = Part('Device', 'C', value='22uF/10V', footprint='C_0805_HandSoldering')
-    vreg['IN'] += vin, inC[1]
-    vreg['GND'] += gnd, inC[2], outC[2]
-    vreg['OUT'] += vout, outC[1]
+    vreg.IN += vin, inC[1]
+    vreg.GND += gnd, inC[2], outC[2]
+    vreg.OUT += vout, outC[1]
 
 # 1v2 supply switching regulator + filters
 @subcircuit
@@ -112,19 +110,79 @@ def reg1v2(vin, vout):
     vreg = Part(local, 'LDL112_SO8', value='LDL112D12R', footprint='SOIC-8_3.9x4.9mm_Pitch1.27mm')
     inC = Part('Device', 'C', value='1uF', footprint='C_0805_HandSoldering')
     outC = Part('Device', 'C', value='1uF', footprint='C_0805_HandSoldering')
-    vreg['IN'] += vin, inC[1]
-    vreg['GND'] += gnd
+    vreg.VIN += vin, inC[1]
+    vreg.GND += gnd
     gnd += inC[2], outC[2]
-    vreg['OUT'] += vout, outC[1]
-    vreg['EN'] += vin    # tie enable high
+    vreg.VOUT += vout, outC[1]
+    vreg.EN += vin    # tie enable high
 
 reg3v3(vin, supply_3v3)
 reg1v2(supply_3v3, supply_1v2)
 
+@subcircuit
+def add0805Pullup(vcc, pin, value):
+    pullup = Part('Device', 'R', value=value, footprint='C_0805_HandSoldering')
+    vcc += pullup[1]
+    pin += pullup[2]
+
+#fpga
+@subcircuit
+def makeFPGA():
+    fpga = Part('Lattice_iCE_FPGA', 'iCE40-HX4K-TQ144', footprint='TQFP-144_20x20mm_Pitch0.5mm')
+
+    # TODO supplies and filters
+    add0805Pullup(fpga['VCCIO_2[2]'], fpga.CRESET_B, '10KOhm')
+    add0805Pullup(fpga['VCCIO_2[2]'], fpga.CDONE, '2.2KOhm')
+
+    return fpga
+
+@subcircuit
+def configEeprom(fpga):
+    global gnd
+
+    eeprom = Part('Memory_EEPROM', '25LCxxx', value='AT25SF041-SSHD-B', footprint='SOIC-8_3.9x4.9mm_Pitch1.27mm')
+    fpga.IOB_107_SCK += eeprom.SCK
+    fpga.IOB_105_SDO += eeprom.MOSI
+    fpga.IOB_106_SDI += eeprom.MISO
+
+    fpga.IOB_108_SS += eeprom['~CS']
+    add0805Pullup(fpga.VCC_SPI, fpga.IOB_108_SS, '10KOhm')
+
+    # tie WP/HOLD high
+    WP_up = Part('Device', 'R', value='10KOhm', footprint='C_0805_HandSoldering')
+    eeprom['~WP'] += WP_up[1]
+    eeprom['~HOLD'] += WP_up[1]
+    eeprom.VCC += WP_up[2]
+
+    eeprom.VCC += fpga.VCC_SPI
+    eeprom.GND += gnd
+    C = Part('Device', 'C', value='100nF', footprint='C_0603_HandSoldering')
+    C[1] += eeprom.VCC
+    C[2] += eeprom.GND
+
+@subcircuit
+def programmingHeader(fpga):
+    hdr = Part('Connector_Generic', 'Conn_02x04_Odd_Even', value='Programming Header', footprint='Pin_Header_Straight_2x04_Pitch2.54mm')
+    hdr[1] += fpga.VCC_SPI
+    hdr[2] += gnd
+    hdr[3] += fpga.CDONE
+    hdr[4] += fpga.CRESET_B
+    hdr[5] += fpga.IOB_105_SDO
+    hdr[6] += fpga.IOB_106_SDI
+    hdr[7] += fpga.IOB_107_SCK
+    hdr[8] += fpga.IOB_108_SS
+
+fpga = makeFPGA()
+
+# Configuration / Programming
+fpga.VCC_SPI += supply_3v3
+configEeprom(fpga)
+programmingHeader(fpga)
+
 # crap here
 cpu = Part('65xx', 'WD65C816S', footprint='PLCC44')
-fpga = Part('Lattice_iCE_FPGA', 'iCE40-HX4K-TQ144', footprint='TQFP-144_20x20mm_Pitch0.5mm')
-cpu['VCC'] += supply_3v3
+acia = Part('65xx', 'W65C51N', footprint='PLCC28')
+cpu.VCC += supply_3v3
 
 data = Bus('data', 8)
 
@@ -132,6 +190,5 @@ cpu['D[0:7]'] += data
 fpga['IOT_[168:177]'] += data
 
 
-if __name__ == "__main__":
-    ERC()
-    generate_netlist()
+#ERC()
+#generate_netlist()
